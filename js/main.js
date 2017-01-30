277f0211dcc2535548be3b12cf311b8ccd329e6d
nicholas hays <nicholas.hays@hotmail.com>
2017-01-29 23:25:19 -0800

    testing gh-pages

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var game = new Phaser.Game(800,600,Phaser.CANVAS, '', {preload: preload, create: create, update: update});
 
 var preload = function() {
-	//TO DO
+	// some preload code
 }
 
 var create = function() {
