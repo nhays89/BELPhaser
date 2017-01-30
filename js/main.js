9165c928f64b1bd6da9b6ef6643691365ab80d74
nicholas hays <nicholas.hays@hotmail.com>
2017-01-29 23:07:51 -0800

    init commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(800,600,Phaser.CANVAS, '', {preload: preload, create: create, update: update});
+
+var preload = function() {
+	//TO DO
+}
+
+var create = function() {
+	//TO DO	
+}
+
+var update = function() {
+	//TO DO
+}
