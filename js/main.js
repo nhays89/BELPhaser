013f7b2fc375d304a8a43dc8fa91dbb31091c326
nicholas hays <nicholas.hays@hotmail.com>
2017-01-30 23:25:07 -0800

    created phaser game object in onload event

--- js/main.js
+++ js/main.js
@@ -4,11 +4,17 @@ var preload = function() {
 }
 
 var create = function() {
-	//TO DO	
+	/*game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;*/
 }
 
 var update = function() {
 	//TO DO
 }
 
-var game = new Phaser.Game(800,600,Phaser.CANVAS, '', {preload: preload, create: create, update: update});
+var width = this.innerWidth;
+var height = this.innerHeight;
+
+window.onload = function() {
+	var game = new Phaser.Game("100%","100%",Phaser.CANVAS, '', {preload: preload, create: create, update: update});
+}
+
