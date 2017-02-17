00a007d810d9ee78b9ea9d13f1be08a374f54c7c
nicholas hays <nicholas.hays@hotmail.com>
2017-02-17 01:00:21 -0800

    added camera movement

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
-
 var preload = function() {
-	
+    this.load.image("bg", "./assets/switchback.jpg", true);
+}
+
+var bgPress = function(e) {
+    var camX = this.game.camera.x;
+    var camY = this.game.camera.y;
+    if (e.key === "a") {
+        console.log("a");
+    } else if (e.key === "s") {
+        camY += (bgHeight - (camY + canvasHeight)) % 10;
+    } else if (e.key === "d") {
+        console.log("d");
+    } else if (e.key === "w") {
+        camY -= camY % 10;
+    }
+}
+var init = function() {
+    this.game.add.image(0, 0, "bg");
+    bgWidth = this.game.cache.getImage("bg").width;
+    bgHeight = this.game.cache.getImage("bg").height;
+    this.game.input.keyboard.onDownCallback = bgPress;
+    this.game.world.bounds.width = bgWidth;
+    this.game.world.bounds.height = bgHeight;
+    this.game.camera.setBoundsToWorld();
 }
 
 var create = function() {
-	/*game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;*/
+    init();
 }
-
 var update = function() {
-	//TO DO
+    console.log("new frame");
 }
 
-var width = this.innerWidth;
-var height = this.innerHeight;
-
+var canvasWidth = this.innerWidth;
+var canvasHeight = this.innerHeight;
+var bgWidth;
+var bgHeight;
+var game;
+var loader;
 window.onload = function() {
-	var game = new Phaser.Game("100%","100%",Phaser.CANVAS, '', {preload: preload, create: create, update: update});
+    game = new Phaser.Game("100%", "100%", Phaser.CANVAS, '', { preload: preload, create: create, update: update });
 }
-
