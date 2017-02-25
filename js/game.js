12ad3d9d8998cb67caa0b5d972ded31f554fa3ad
nicholas hays <nicholas.hays@hotmail.com>
2017-02-25 00:36:52 -0800

    modified file struct

--- js/game.js
+++ js/game.js
@@ -1,6 +1,5 @@
 var game = new Phaser.Game("100%", "100%", Phaser.CANVAS, '', null,true,false);
 
-//game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
 game.state.add('play', playState);
