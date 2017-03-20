341da4b05dba7e5319a6d4fc849f87b5f793e89d
nicholas hays <nicholas.hays@hotmail.com>
2017-03-20 02:10:30 -0700

    added sound

--- js/load.js
+++ js/load.js
@@ -4,6 +4,20 @@ var loadState = {
         var loadingLabel = game.add.text(200, 200, 'Loading...',
             {font: '30px Courier', fill: '#000000'});
 
+        //sound
+        game.load.audio('intro-dramatic-effect', 'assets/sound/intro-dramatic-effect.wav');
+        game.load.audio('american-gun-shot', 'assets/sound/american-gun-shot.wav');
+        game.load.audio('soviet-gun-shot', 'assets/sound/soviet-gun-shot.wav');
+        game.load.audio('american-march', 'assets/sound/american-march.wav');
+        game.load.audio('intro', 'assets/sound/intro.wav');
+        game.load.audio('american-dying', 'assets/sound/american-dying.wav');
+        game.load.audio('soviet-dying', 'assets/sound/soviet-dying.wav');
+        game.load.audio('bombs', 'assets/sound/bombs.wav');
+        game.load.audio("roger", 'assets/sound/roger.wav');
+       game.load.audio('kill-beep', 'assets/sound/kill-beep.wav');
+       game.load.audio('move-beep', 'assets/sound/move-beep.wav');
+
+
         //map files
         game.load.image('town', 'assets/map/town.png');
         game.load.image('Castle', 'assets/map/Castle.png');
